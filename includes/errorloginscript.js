21b7e97d34dac713c70ab098f324eefcbd75cff5
toodams <63766963+toodams@users.noreply.github.com>
2022-01-03 18:23:50 +0100

    added more files

---
+++ includes/errorloginscript.js
@@ -0,0 +1,9 @@
+const errormessing = document.querySelector(".error_msg");
+const xbtnmessing = document.querySelector(".error-x-btn");
+
+
+
+xbtnmessing.addEventListener("click", () => {
+    errormessing.classList.add("errorcancle");
+
+});
